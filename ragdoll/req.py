0026d7587bc21688f6000599d6ad6dea5df05055
Sen Wang <wangsen992@gmail.com>
2017-07-02 01:19:59 +0800

    initiate work on req objects

---
+++ ragdoll/req.py
@@ -0,0 +1,11 @@
+"""
+This is a script to faciliate the construction of Requirement objects.
+"""
+
+from db import Mongo
+from composite import Nutrient, Nutrients
+
+class Requirement(Nutrients):
+
+	def __init__(self, )
+
