79cab91a91e39a91ddd8df24d6be2a6485e2a0be
yiliu9090 <Yi.liu9090@gmail.com>
2017-07-03 23:32:12 -0500

    Optimzation package build

---
+++ Optim/__init__.py
@@ -0,0 +1,13 @@
+"""
+Ragdoll initialisation.
+"""
+
+from .db import *
+from .composite import *
+from .dictionary import *
+from .human import *
+from .req import *
+from .plots import *
+
+
+__version__=0.1
