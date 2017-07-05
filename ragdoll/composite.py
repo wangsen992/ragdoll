f299971c60335c0a6b2a6b0dcf84f244dca1b46e
Sen Wang <wangsen992@gmail.com>
2017-07-05 20:56:55 +0800

    updated the nutrient object with amount objects

--- ragdoll/composite.py
+++ ragdoll/composite.py
@@ -13,16 +13,19 @@ should be abstracted with plus and minus signs for simple manipulation.
 By separating the nutrients and the ingredients, it is possible for future
 extension with more databases. 
 """
+from collections import defaultdict, OrderedDict
+
 import pandas as pd
 
-from collections import defaultdict, OrderedDict
- 
+from .amount import Amount
 
 # format string used for representation of things
 title_format_str = "{abbr:<10s} {value:<10} {unit:<10s} {name:<15s}\n"
 entry_format_str = "{abbr:<10s} {value:<10.2f} {unit:<10s} {name:<15s}\n"
 recipe_title_format_str = "{index:<5} {value:<10} {unit:<5s} {db:10s} {name: <20s}\n"
 recipe_entry_format_str = "{index:<5} {value:<10.1f} {unit:<5s} {db:10s} {name: <20s}\n"
+
+
 
 class Nutrient(object):
 	"""A basic concrete class for handling nutrient-level operations.
@@ -67,10 +70,10 @@ class Nutrient(object):
 		"""
 
 		self.name = name
-		self.value = value
-		self.unit = unit
+		self.amt = Amount(value, unit)
 		self.abbr = abbr
 		self.name_source=name_source
+
 		self.source = set()
 		if type(source) == str:
 			self.source.add(source)
@@ -102,19 +105,17 @@ class Nutrient(object):
 		source = self.source.copy()
 		source.update(other.source)
 
+		new_amt = self.amt + other.amt
+
 		return Nutrient(name=self.name,
-						value=self.value + other.value,
-						unit=self.unit,
+						value=new_amt.value,
+						unit=new_amt.unit,
 						abbr=self.abbr,
 						source=source,
 						name_source=self.name_source)
 
 	def __sub__(self, other):
 		"""Subtraction of another Nutrient object.
-
-		Note
-		----
-		This method currently enforces that : self.value >= other.value.
 
 		Parameters
 		----------
@@ -133,15 +134,14 @@ class Nutrient(object):
 
 		assert self.__type_test(other), "Type mismatch between two nutrient objects."
 
-		assert (self.value >= other.value), "First nutrient value smaller than the second."
-
 		source = self.source.copy()
 		source.update(other.source)
 
+		new_amt = self.amt - other.amt
 
 		return Nutrient(name=self.name,
-						value=self.value - other.value,
-						unit=self.unit,
+						value=new_amt.value,
+						unit=new_amt.unit,
 						abbr=self.abbr,
 						source=source,
 						name_source=self.name_source)
@@ -166,17 +166,19 @@ class Nutrient(object):
 
 		"""
 
-		if type(scalar) not in [int, float]:
-			raise ValueError("Must be multiplied with a scalar.")
+		if type(scalar) in [int, float]:
 
-		assert (scalar >= 0), "Scalar must be equal or larger than zero!"
+			new_amt = self.amt * scalar
 
-		return Nutrient(name=self.name,
-						value=self.value * scalar,
-						unit=self.unit,
-						abbr=self.abbr,
-						source=self.source,
-						name_source=self.name_source)
+			return Nutrient(name=self.name,
+							value=new_amt.value,
+							unit=new_amt.unit,
+							abbr=self.abbr,
+							source=self.source,
+							name_source=self.name_source)
+
+		else:
+			raise TypeError("Nutrient object must be multiplied with a scalar.")
 
 	def __rmul__(self, scalar):
 		"""Reverse multiplication. 
@@ -231,238 +233,22 @@ class Nutrient(object):
 		"""
 
 		if type(other) in [int, float]:
-			assert (other > 0), "Scalar must be larger than zero!"
+
+			new_amt = self.amt / other
 
 			return Nutrient(name=self.name,
-							value=self.value / other,
-							unit=self.unit,
+							value=new_amt.value,
+							unit=new_amt.unit,
 							abbr=self.abbr,
 							source=self.source,
 							name_source=self.name_source)
 
 		elif self.__type_test(other):
 
-			return Nutrient(name=self.name,
-							value=self.value / other.value,
-							unit=self.unit,
-							abbr=self.abbr,
-							source=self.source,
-							name_source=self.name_source)
+			return self.amt / other.amt
 
 		else:
 			raise TypeError("Must be divided with a scalar or Nutrient object of the same type.")
-
-		
-
-	def __floordiv__(self, other):
-		"""Floor division by a scalar or Nutrient object of same type.
-		
-		Note
-		----
-		This method enforces that : scalar > 0
-
-		Parameters
-		----------
-		other : float/int or Nutrient
-			* If type(other) in [float, int], perform division on self.value
-			  with other.
-			* If type(other) == Nutrient, after type check, division is 
-			  performed by divided self.value with other.value
-
-		if scalar provided, returns a Nutrient object;
-		if Nutrient object of same type provided, returns a scalar.
-
-		Returns
-		-------
-		Nutrient:
-			A Nutrient object of same type (name, unit, abbr), with value being
-			self.value divided by the scalar or other.value.
-		"""
-
-		if type(other) in [int, float]:
-			assert (other > 0), "Scalar must be larger than zero!"
-
-			return Nutrient(name=self.name,
-							value=self.value // other,
-							unit=self.unit,
-							abbr=self.abbr,
-							source=self.source,
-							name_source=self.name_source)
-
-		elif self.__type_test(other):
-
-			return self.value // other.value
-
-		else:
-			raise TypeError("Must be divided with a scalar or Nutrient object of the same type.")
-
-	def __mod__(self, other):
-		"""modulus by a scalar or Nutrient object of same type.
-
-		Note
-		----
-		This method enforces that : scalar > 0
-
-		Parameters
-		----------
-		other : float/int or Nutrient
-			* If type(other) in [float, int], perform division on self.value
-			  with other.
-			* If type(other) == Nutrient, after type check, division is 
-			  performed by divided self.value with other.value
-
-		if scalar provided, returns a Nutrient object;
-		if Nutrient object of same type provided, returns a scalar.
-
-		Returns
-		-------
-		Nutrient:
-			A Nutrient object of same type (name, unit, abbr), with value being
-			self.value modularized by the scalar or other.value.
-		"""
-
-		if type(other) in [int, float]:
-			assert (other > 0), "Scalar must be larger than zero!"
-
-			return Nutrient(name=self.name,
-							value=self.value % other,
-							unit=self.unit,
-							abbr=self.abbr,
-							source=self.source,
-							name_source=self.name_source)
-
-		elif self.__type_test(other):
-
-			return self.value % other.value
-
-		else:
-			raise TypeError("Must be modularized with a scalar or Nutrient object of the same type.")
-
-	def __lt__(self, other):
-		"""Check less than condition with another Nutrient.
-
-		Parameters
-		----------
-		other : Nutrient
-			The other Nutrient object to be compared with. Enforcement on the type of
-			nutrient required. Currently it is based on equality of name, unit
-			and abbr.
-
-		Returns
-		-------
-		bool
-			True if less than, False otherwise.
-
-		"""
-
-		if self.__type_test(other):
-
-			return self.value < other.value
-
-	def __le__(self, other):
-		"""Check less than or equal to condition with another Nutrient.
-
-		Parameters
-		----------
-		other : Nutrient
-			The other Nutrient object to compared with. Enforcement on the type of
-			nutrient required. Currently it is based on equality of name, unit
-			and abbr.
-
-		Returns
-		-------
-		bool
-			True if less than or equal to, False otherwise.
-
-		"""
-
-		if self.__type_test(other):
-
-			return self.value <= other.value
-
-	def __eq__(self, other):
-		"""Check equal to condition with another Nutrient.
-
-		Parameters
-		----------
-		other : Nutrient
-			The other Nutrient object to compared with. Enforcement on the type of
-			nutrient required. Currently it is based on equality of name, unit
-			and abbr.
-
-		Returns
-		-------
-		bool
-			True if equal, False otherwise.
-
-		"""
-
-		if self.__type_test(other):
-
-			return self.value == other.value
-
-	def __ne__(self, other):
-		"""Check unequal to condition with another Nutrient.
-
-		Parameters
-		----------
-		other : Nutrient
-			The other Nutrient object to compared with. Enforcement on the type of
-			nutrient required. Currently it is based on equality of name, unit
-			and abbr.
-
-		Returns
-		-------
-		bool
-			True if unequal, False otherwise.
-
-		"""
-
-		if self.__type_test(other):
-
-			return self.value != other.value
-
-	def __ge__(self, other):
-		"""Check greater than or equal to condition with another Nutrient.
-
-		Parameters
-		----------
-		other : Nutrient
-			The other Nutrient object to compared with. Enforcement on the type of
-			nutrient required. Currently it is based on equality of name, unit
-			and abbr.
-
-		Returns
-		-------
-		bool
-			True if greater than or equal to, False otherwise.
-
-		"""
-
-		if self.__type_test(other):
-
-			return self.value >= other.value
-
-	def __gt__(self, other):
-		"""Check greater than condition with another Nutrient.
-
-		Parameters
-		----------
-		other : Nutrient
-			The other Nutrient object to compared with. Enforcement on the type of
-			nutrient required. Currently it is based on equality of name, unit
-			and abbr.
-
-		Returns
-		-------
-		bool
-			True if greater, False otherwise.
-
-		"""
-
-		if self.__type_test(other):
-
-			return self.value > other.value
 
 	# Functions for emulating container types.
 
@@ -475,8 +261,8 @@ class Nutrient(object):
 										    unit='UNIT')
 		entry_str = entry_format_str.format(abbr=self.abbr,
 										    name=self.name,
-										    value=self.value,
-										    unit=self.unit)
+										    value=self.amt.value,
+										    unit=self.amt.unit)
 		return title_str + entry_str
 
 	def __type_test(self, other):
@@ -501,27 +287,9 @@ class Nutrient(object):
 
 		if type(other) != Nutrient:
 			raise TypeError("Second argument must be a Nutrient object.")
-			return False
-
-		if self.name != other.name:
-			raise ValueError("Nutrient names not the same.")
-			return False
 
 		elif self.abbr != other.abbr:
-			print(self.name)
-			print(self.abbr)
-			print(other.name)
-			print(other.abbr)
 			raise ValueError("Abbreviations not the same.")
-			return False
-
-		elif self.unit != other.unit:
-			print(self.name)
-			print(self.unit)
-			print(other.name)
-			print(other.unit)
-			raise ValueError("Unit types not the same.")
-			return False
 
 		return True
 
